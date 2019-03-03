cd1227cd216682758918ffb2c2e6d9a135bdf1fe
Kalkidan Lemma <k.lemma14@gmail.com>
2019-03-03 02:15:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,3 +26,9 @@ function makeInt(n) {
 function makeInt(n) {
   return parseInt('fdfd', 10)
 }
+function preserveDecimal(n) {
+  return parseFloat(n, 10);
+  return parseInt(n, 10);
+}
+function 
+}
